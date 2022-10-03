eba9f0db1bc1b87b0da0a49557f50f054b8f1fd9
Peter Fiee <peterfiee@icloud.com>
2022-10-03 01:10:55 -0400

    add CoffeeMaker class

--- coffee_machine/main.py
+++ coffee_machine/main.py
@@ -1,31 +1,66 @@
-from menu import beverage, resources
+from properties import beverage, resources
 
 
-def report():
-    for key, item in resources.items():
-        print(f"{key.capitalize()}: {item}")
+class CoffeeMaker:
+
+    def __init__(self, drink: str, money: float = 2.50) -> None:
+        self.drink = drink
+        self.water, self.milk, self.coffee = [*resources.values()]
+        self.money = money
+
+
+    def make_coffee(self):
+        water, milk, coffee = beverage.get(self.drink).get('ingredients').values()
+
+        def check_resource(self):
+            if water > self.water:
+                print("Insufficient Water")
+            elif milk > self.milk:
+                print("Insufficient Milk")
+            elif coffee > self.coffee:
+                print("Insufficient coffee")
+            return self.water >= water and self.milk >= milk and self.coffee >= coffee
+
+        if check_resource(self):
+            print(f"Brewing a delicous {self.drink.capitalize()}...")
+            self.water -= water
+            self.milk -= milk
+            self.coffee -= coffee
+
+    def status_report(self):
+        resource_levels = (
+        "Coffee Maker Resource Levels:\n"
+        f"Water : {self.water}ml\n"
+        f"Milk  : {self.milk}ml\n"
+        f"Coffee: {self.coffee}g\n"
+        f"Money : ${self.money:.2f}")
+
+        print(resource_levels)
+
 
 
 def get_drink() -> str:
-    choice = input("What would you like? (cappuccino/espresso/latte): ").lower()
+    choice = input("What would you like? (Cappuccino/Espresso/Latte): ").lower()
 
     while choice not in beverage.keys():
-        print(f"Sorry. {choice.capitalize()} not available!")
-        choice = input("What would you like? (cappuccino/espresso/latte): ").lower()
-        if choice == 'q':
-            print("Goodbye")
+        if choice in ('q', 'Q'):
+            print("Exiting...")
             return
+        print(f"Sorry. {choice} not offered at the moment!")
+        choice = input("Choose either Cappuccino/Espresso/Latte): ").lower()
 
     return choice
 
 
-def check_resource(drink):
-    print(f"You chose {drink}")
-
-
 def main():
+
     user_drink = get_drink()
-    check_resource(drink=user_drink)
+    if user_drink:
+        machine = CoffeeMaker(user_drink)
+        machine.status_report()
+        print(machine.water)
+        machine.make_coffee()
+        machine.status_report()
 
 
 if __name__ == "__main__":
